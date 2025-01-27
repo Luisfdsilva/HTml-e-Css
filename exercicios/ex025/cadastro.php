b05486afc3c65a0fec7b2ab4fc5f478624f719f3
luis silva <tpd.luix@gmail.com>
2025-01-27 16:58:12 -0300

    Update cadastro.php

--- exercicios/ex025/cadastro.php
+++ exercicios/ex025/cadastro.php
@@ -1,2 +1,4 @@
 os dadosforam enviados!
-Aprenda PHPpara saber mais
+Aprenda PHP para saber mais sobre essa linguagem
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi, veniam officia! Hic, repellendus excepturi eius obcaecati, voluptatibus, voluptatem dignissimos quisquam ex illum dolores ipsum! Labore nam inventore amet! Impedit,
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi, veniam officia! Hic, repellendus excepturi eius obcaecati, voluptatibus, voluptatem dignissimos quisquam ex illum dolores ipsum! Labore nam inventore amet! Impedit,
